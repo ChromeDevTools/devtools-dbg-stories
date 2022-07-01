068f84c32fade7776758564d2263b1f7a48ba242
Benedikt Meurer <bmeurer@google.com>
2022-07-01 12:53:13 +0200

    feat(crbug/1328681): add repros for TDZ and optimized_out

---
+++ src/crbug-1328681-value-unavailable.js
@@ -0,0 +1,22 @@
+function tdz() {
+  debugger;
+  const x = 2 ?? y + x;
+  let y = x + 1;
+  return y;
+}
+
+function opt() {
+  function optimizedOut(x, stop) {
+    let y = x + 1;
+    let z = y + 1;
+    if (stop) {
+      debugger;
+    }
+    return z;
+  }
+
+  for (let n = 0; n < 100000; ++n) {
+    optimizedOut(n, false);
+  }
+  return optimizedOut(1, true);
+}

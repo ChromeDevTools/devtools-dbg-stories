cf47048f00b9310f9734a1607c988315dc83f6bb
Benedikt Meurer <bmeurer@google.com>
2022-07-04 07:50:24 +0200

    feat(crbug/1341602): add repro for crbug.com/1341602

---
+++ src/crbug-1341602-bar.js
@@ -0,0 +1,600 @@
+function bar1() {
+  console.log(1);
+}
+function bar2() {
+  console.log(2);
+}
+function bar3() {
+  console.log(3);
+}
+function bar4() {
+  console.log(4);
+}
+function bar5() {
+  console.log(5);
+}
+function bar6() {
+  console.log(6);
+}
+function bar7() {
+  console.log(7);
+}
+function bar8() {
+  console.log(8);
+}
+function bar9() {
+  console.log(9);
+}
+function bar10() {
+  console.log(10);
+}
+function bar11() {
+  console.log(11);
+}
+function bar12() {
+  console.log(12);
+}
+function bar13() {
+  console.log(13);
+}
+function bar14() {
+  console.log(14);
+}
+function bar15() {
+  console.log(15);
+}
+function bar16() {
+  console.log(16);
+}
+function bar17() {
+  console.log(17);
+}
+function bar18() {
+  console.log(18);
+}
+function bar19() {
+  console.log(19);
+}
+function bar20() {
+  console.log(20);
+}
+function bar21() {
+  console.log(21);
+}
+function bar22() {
+  console.log(22);
+}
+function bar23() {
+  console.log(23);
+}
+function bar24() {
+  console.log(24);
+}
+function bar25() {
+  console.log(25);
+}
+function bar26() {
+  console.log(26);
+}
+function bar27() {
+  console.log(27);
+}
+function bar28() {
+  console.log(28);
+}
+function bar29() {
+  console.log(29);
+}
+function bar30() {
+  console.log(30);
+}
+function bar31() {
+  console.log(31);
+}
+function bar32() {
+  console.log(32);
+}
+function bar33() {
+  console.log(33);
+}
+function bar34() {
+  console.log(34);
+}
+function bar35() {
+  console.log(35);
+}
+function bar36() {
+  console.log(36);
+}
+function bar37() {
+  console.log(37);
+}
+function bar38() {
+  console.log(38);
+}
+function bar39() {
+  console.log(39);
+}
+function bar40() {
+  console.log(40);
+}
+function bar41() {
+  console.log(41);
+}
+function bar42() {
+  console.log(42);
+}
+function bar43() {
+  console.log(43);
+}
+function bar44() {
+  console.log(44);
+}
+function bar45() {
+  console.log(45);
+}
+function bar46() {
+  console.log(46);
+}
+function bar47() {
+  console.log(47);
+}
+function bar48() {
+  console.log(48);
+}
+function bar49() {
+  console.log(49);
+}
+function bar50() {
+  console.log(50);
+}
+function bar51() {
+  console.log(51);
+}
+function bar52() {
+  console.log(52);
+}
+function bar53() {
+  console.log(53);
+}
+function bar54() {
+  console.log(54);
+}
+function bar55() {
+  console.log(55);
+}
+function bar56() {
+  console.log(56);
+}
+function bar57() {
+  console.log(57);
+}
+function bar58() {
+  console.log(58);
+}
+function bar59() {
+  console.log(59);
+}
+function bar60() {
+  console.log(60);
+}
+function bar61() {
+  console.log(61);
+}
+function bar62() {
+  console.log(62);
+}
+function bar63() {
+  console.log(63);
+}
+function bar64() {
+  console.log(64);
+}
+function bar65() {
+  console.log(65);
+}
+function bar66() {
+  console.log(66);
+}
+function bar67() {
+  console.log(67);
+}
+function bar68() {
+  console.log(68);
+}
+function bar69() {
+  console.log(69);
+}
+function bar70() {
+  console.log(70);
+}
+function bar71() {
+  console.log(71);
+}
+function bar72() {
+  console.log(72);
+}
+function bar73() {
+  console.log(73);
+}
+function bar74() {
+  console.log(74);
+}
+function bar75() {
+  console.log(75);
+}
+function bar76() {
+  console.log(76);
+}
+function bar77() {
+  console.log(77);
+}
+function bar78() {
+  console.log(78);
+}
+function bar79() {
+  console.log(79);
+}
+function bar80() {
+  console.log(80);
+}
+function bar81() {
+  console.log(81);
+}
+function bar82() {
+  console.log(82);
+}
+function bar83() {
+  console.log(83);
+}
+function bar84() {
+  console.log(84);
+}
+function bar85() {
+  console.log(85);
+}
+function bar86() {
+  console.log(86);
+}
+function bar87() {
+  console.log(87);
+}
+function bar88() {
+  console.log(88);
+}
+function bar89() {
+  console.log(89);
+}
+function bar90() {
+  console.log(90);
+}
+function bar91() {
+  console.log(91);
+}
+function bar92() {
+  console.log(92);
+}
+function bar93() {
+  console.log(93);
+}
+function bar94() {
+  console.log(94);
+}
+function bar95() {
+  console.log(95);
+}
+function bar96() {
+  console.log(96);
+}
+function bar97() {
+  console.log(97);
+}
+function bar98() {
+  console.log(98);
+}
+function bar99() {
+  console.log(99);
+}
+function bar100() {
+  console.log(100);
+}
+function bar101() {
+  console.log(101);
+}
+function bar102() {
+  console.log(102);
+}
+function bar103() {
+  console.log(103);
+}
+function bar104() {
+  console.log(104);
+}
+function bar105() {
+  console.log(105);
+}
+function bar106() {
+  console.log(106);
+}
+function bar107() {
+  console.log(107);
+}
+function bar108() {
+  console.log(108);
+}
+function bar109() {
+  console.log(109);
+}
+function bar110() {
+  console.log(110);
+}
+function bar111() {
+  console.log(111);
+}
+function bar112() {
+  console.log(112);
+}
+function bar113() {
+  console.log(113);
+}
+function bar114() {
+  console.log(114);
+}
+function bar115() {
+  console.log(115);
+}
+function bar116() {
+  console.log(116);
+}
+function bar117() {
+  console.log(117);
+}
+function bar118() {
+  console.log(118);
+}
+function bar119() {
+  console.log(119);
+}
+function bar120() {
+  console.log(120);
+}
+function bar121() {
+  console.log(121);
+}
+function bar122() {
+  console.log(122);
+}
+function bar123() {
+  console.log(123);
+}
+function bar124() {
+  console.log(124);
+}
+function bar125() {
+  console.log(125);
+}
+function bar126() {
+  console.log(126);
+}
+function bar127() {
+  console.log(127);
+}
+function bar128() {
+  console.log(128);
+}
+function bar129() {
+  console.log(129);
+}
+function bar130() {
+  console.log(130);
+}
+function bar131() {
+  console.log(131);
+}
+function bar132() {
+  console.log(132);
+}
+function bar133() {
+  console.log(133);
+}
+function bar134() {
+  console.log(134);
+}
+function bar135() {
+  console.log(135);
+}
+function bar136() {
+  console.log(136);
+}
+function bar137() {
+  console.log(137);
+}
+function bar138() {
+  console.log(138);
+}
+function bar139() {
+  console.log(139);
+}
+function bar140() {
+  console.log(140);
+}
+function bar141() {
+  console.log(141);
+}
+function bar142() {
+  console.log(142);
+}
+function bar143() {
+  console.log(143);
+}
+function bar144() {
+  console.log(144);
+}
+function bar145() {
+  console.log(145);
+}
+function bar146() {
+  console.log(146);
+}
+function bar147() {
+  console.log(147);
+}
+function bar148() {
+  console.log(148);
+}
+function bar149() {
+  console.log(149);
+}
+function bar150() {
+  console.log(150);
+}
+function bar151() {
+  console.log(151);
+}
+function bar152() {
+  console.log(152);
+}
+function bar153() {
+  console.log(153);
+}
+function bar154() {
+  console.log(154);
+}
+function bar155() {
+  console.log(155);
+}
+function bar156() {
+  console.log(156);
+}
+function bar157() {
+  console.log(157);
+}
+function bar158() {
+  console.log(158);
+}
+function bar159() {
+  console.log(159);
+}
+function bar160() {
+  console.log(160);
+}
+function bar161() {
+  console.log(161);
+}
+function bar162() {
+  console.log(162);
+}
+function bar163() {
+  console.log(163);
+}
+function bar164() {
+  console.log(164);
+}
+function bar165() {
+  console.log(165);
+}
+function bar166() {
+  console.log(166);
+}
+function bar167() {
+  console.log(167);
+}
+function bar168() {
+  console.log(168);
+}
+function bar169() {
+  console.log(169);
+}
+function bar170() {
+  console.log(170);
+}
+function bar171() {
+  console.log(171);
+}
+function bar172() {
+  console.log(172);
+}
+function bar173() {
+  console.log(173);
+}
+function bar174() {
+  console.log(174);
+}
+function bar175() {
+  console.log(175);
+}
+function bar176() {
+  console.log(176);
+}
+function bar177() {
+  console.log(177);
+}
+function bar178() {
+  console.log(178);
+}
+function bar179() {
+  console.log(179);
+}
+function bar180() {
+  console.log(180);
+}
+function bar181() {
+  console.log(181);
+}
+function bar182() {
+  console.log(182);
+}
+function bar183() {
+  console.log(183);
+}
+function bar184() {
+  console.log(184);
+}
+function bar185() {
+  console.log(185);
+}
+function bar186() {
+  console.log(186);
+}
+function bar187() {
+  console.log(187);
+}
+function bar188() {
+  console.log(188);
+}
+function bar189() {
+  console.log(189);
+}
+function bar190() {
+  console.log(190);
+}
+function bar191() {
+  console.log(191);
+}
+function bar192() {
+  console.log(192);
+}
+function bar193() {
+  console.log(193);
+}
+function bar194() {
+  console.log(194);
+}
+function bar195() {
+  console.log(195);
+}
+function bar196() {
+  console.log(196);
+}
+function bar197() {
+  console.log(197);
+}
+function bar198() {
+  console.log(198);
+}
+function bar199() {
+  console.log(199);
+}
+function bar200() {
+  console.log(200);
+}
---
+++ src/crbug-1341602-foo.js
@@ -0,0 +1,600 @@
+function foo1() {
+  console.log(1);
+}
+function foo2() {
+  console.log(2);
+}
+function foo3() {
+  console.log(3);
+}
+function foo4() {
+  console.log(4);
+}
+function foo5() {
+  console.log(5);
+}
+function foo6() {
+  console.log(6);
+}
+function foo7() {
+  console.log(7);
+}
+function foo8() {
+  console.log(8);
+}
+function foo9() {
+  console.log(9);
+}
+function foo10() {
+  console.log(10);
+}
+function foo11() {
+  console.log(11);
+}
+function foo12() {
+  console.log(12);
+}
+function foo13() {
+  console.log(13);
+}
+function foo14() {
+  console.log(14);
+}
+function foo15() {
+  console.log(15);
+}
+function foo16() {
+  console.log(16);
+}
+function foo17() {
+  console.log(17);
+}
+function foo18() {
+  console.log(18);
+}
+function foo19() {
+  console.log(19);
+}
+function foo20() {
+  console.log(20);
+}
+function foo21() {
+  console.log(21);
+}
+function foo22() {
+  console.log(22);
+}
+function foo23() {
+  console.log(23);
+}
+function foo24() {
+  console.log(24);
+}
+function foo25() {
+  console.log(25);
+}
+function foo26() {
+  console.log(26);
+}
+function foo27() {
+  console.log(27);
+}
+function foo28() {
+  console.log(28);
+}
+function foo29() {
+  console.log(29);
+}
+function foo30() {
+  console.log(30);
+}
+function foo31() {
+  console.log(31);
+}
+function foo32() {
+  console.log(32);
+}
+function foo33() {
+  console.log(33);
+}
+function foo34() {
+  console.log(34);
+}
+function foo35() {
+  console.log(35);
+}
+function foo36() {
+  console.log(36);
+}
+function foo37() {
+  console.log(37);
+}
+function foo38() {
+  console.log(38);
+}
+function foo39() {
+  console.log(39);
+}
+function foo40() {
+  console.log(40);
+}
+function foo41() {
+  console.log(41);
+}
+function foo42() {
+  console.log(42);
+}
+function foo43() {
+  console.log(43);
+}
+function foo44() {
+  console.log(44);
+}
+function foo45() {
+  console.log(45);
+}
+function foo46() {
+  console.log(46);
+}
+function foo47() {
+  console.log(47);
+}
+function foo48() {
+  console.log(48);
+}
+function foo49() {
+  console.log(49);
+}
+function foo50() {
+  console.log(50);
+}
+function foo51() {
+  console.log(51);
+}
+function foo52() {
+  console.log(52);
+}
+function foo53() {
+  console.log(53);
+}
+function foo54() {
+  console.log(54);
+}
+function foo55() {
+  console.log(55);
+}
+function foo56() {
+  console.log(56);
+}
+function foo57() {
+  console.log(57);
+}
+function foo58() {
+  console.log(58);
+}
+function foo59() {
+  console.log(59);
+}
+function foo60() {
+  console.log(60);
+}
+function foo61() {
+  console.log(61);
+}
+function foo62() {
+  console.log(62);
+}
+function foo63() {
+  console.log(63);
+}
+function foo64() {
+  console.log(64);
+}
+function foo65() {
+  console.log(65);
+}
+function foo66() {
+  console.log(66);
+}
+function foo67() {
+  console.log(67);
+}
+function foo68() {
+  console.log(68);
+}
+function foo69() {
+  console.log(69);
+}
+function foo70() {
+  console.log(70);
+}
+function foo71() {
+  console.log(71);
+}
+function foo72() {
+  console.log(72);
+}
+function foo73() {
+  console.log(73);
+}
+function foo74() {
+  console.log(74);
+}
+function foo75() {
+  console.log(75);
+}
+function foo76() {
+  console.log(76);
+}
+function foo77() {
+  console.log(77);
+}
+function foo78() {
+  console.log(78);
+}
+function foo79() {
+  console.log(79);
+}
+function foo80() {
+  console.log(80);
+}
+function foo81() {
+  console.log(81);
+}
+function foo82() {
+  console.log(82);
+}
+function foo83() {
+  console.log(83);
+}
+function foo84() {
+  console.log(84);
+}
+function foo85() {
+  console.log(85);
+}
+function foo86() {
+  console.log(86);
+}
+function foo87() {
+  console.log(87);
+}
+function foo88() {
+  console.log(88);
+}
+function foo89() {
+  console.log(89);
+}
+function foo90() {
+  console.log(90);
+}
+function foo91() {
+  console.log(91);
+}
+function foo92() {
+  console.log(92);
+}
+function foo93() {
+  console.log(93);
+}
+function foo94() {
+  console.log(94);
+}
+function foo95() {
+  console.log(95);
+}
+function foo96() {
+  console.log(96);
+}
+function foo97() {
+  console.log(97);
+}
+function foo98() {
+  console.log(98);
+}
+function foo99() {
+  console.log(99);
+}
+function foo100() {
+  console.log(100);
+}
+function foo101() {
+  console.log(101);
+}
+function foo102() {
+  console.log(102);
+}
+function foo103() {
+  console.log(103);
+}
+function foo104() {
+  console.log(104);
+}
+function foo105() {
+  console.log(105);
+}
+function foo106() {
+  console.log(106);
+}
+function foo107() {
+  console.log(107);
+}
+function foo108() {
+  console.log(108);
+}
+function foo109() {
+  console.log(109);
+}
+function foo110() {
+  console.log(110);
+}
+function foo111() {
+  console.log(111);
+}
+function foo112() {
+  console.log(112);
+}
+function foo113() {
+  console.log(113);
+}
+function foo114() {
+  console.log(114);
+}
+function foo115() {
+  console.log(115);
+}
+function foo116() {
+  console.log(116);
+}
+function foo117() {
+  console.log(117);
+}
+function foo118() {
+  console.log(118);
+}
+function foo119() {
+  console.log(119);
+}
+function foo120() {
+  console.log(120);
+}
+function foo121() {
+  console.log(121);
+}
+function foo122() {
+  console.log(122);
+}
+function foo123() {
+  console.log(123);
+}
+function foo124() {
+  console.log(124);
+}
+function foo125() {
+  console.log(125);
+}
+function foo126() {
+  console.log(126);
+}
+function foo127() {
+  console.log(127);
+}
+function foo128() {
+  console.log(128);
+}
+function foo129() {
+  console.log(129);
+}
+function foo130() {
+  console.log(130);
+}
+function foo131() {
+  console.log(131);
+}
+function foo132() {
+  console.log(132);
+}
+function foo133() {
+  console.log(133);
+}
+function foo134() {
+  console.log(134);
+}
+function foo135() {
+  console.log(135);
+}
+function foo136() {
+  console.log(136);
+}
+function foo137() {
+  console.log(137);
+}
+function foo138() {
+  console.log(138);
+}
+function foo139() {
+  console.log(139);
+}
+function foo140() {
+  console.log(140);
+}
+function foo141() {
+  console.log(141);
+}
+function foo142() {
+  console.log(142);
+}
+function foo143() {
+  console.log(143);
+}
+function foo144() {
+  console.log(144);
+}
+function foo145() {
+  console.log(145);
+}
+function foo146() {
+  console.log(146);
+}
+function foo147() {
+  console.log(147);
+}
+function foo148() {
+  console.log(148);
+}
+function foo149() {
+  console.log(149);
+}
+function foo150() {
+  console.log(150);
+}
+function foo151() {
+  console.log(151);
+}
+function foo152() {
+  console.log(152);
+}
+function foo153() {
+  console.log(153);
+}
+function foo154() {
+  console.log(154);
+}
+function foo155() {
+  console.log(155);
+}
+function foo156() {
+  console.log(156);
+}
+function foo157() {
+  console.log(157);
+}
+function foo158() {
+  console.log(158);
+}
+function foo159() {
+  console.log(159);
+}
+function foo160() {
+  console.log(160);
+}
+function foo161() {
+  console.log(161);
+}
+function foo162() {
+  console.log(162);
+}
+function foo163() {
+  console.log(163);
+}
+function foo164() {
+  console.log(164);
+}
+function foo165() {
+  console.log(165);
+}
+function foo166() {
+  console.log(166);
+}
+function foo167() {
+  console.log(167);
+}
+function foo168() {
+  console.log(168);
+}
+function foo169() {
+  console.log(169);
+}
+function foo170() {
+  console.log(170);
+}
+function foo171() {
+  console.log(171);
+}
+function foo172() {
+  console.log(172);
+}
+function foo173() {
+  console.log(173);
+}
+function foo174() {
+  console.log(174);
+}
+function foo175() {
+  console.log(175);
+}
+function foo176() {
+  console.log(176);
+}
+function foo177() {
+  console.log(177);
+}
+function foo178() {
+  console.log(178);
+}
+function foo179() {
+  console.log(179);
+}
+function foo180() {
+  console.log(180);
+}
+function foo181() {
+  console.log(181);
+}
+function foo182() {
+  console.log(182);
+}
+function foo183() {
+  console.log(183);
+}
+function foo184() {
+  console.log(184);
+}
+function foo185() {
+  console.log(185);
+}
+function foo186() {
+  console.log(186);
+}
+function foo187() {
+  console.log(187);
+}
+function foo188() {
+  console.log(188);
+}
+function foo189() {
+  console.log(189);
+}
+function foo190() {
+  console.log(190);
+}
+function foo191() {
+  console.log(191);
+}
+function foo192() {
+  console.log(192);
+}
+function foo193() {
+  console.log(193);
+}
+function foo194() {
+  console.log(194);
+}
+function foo195() {
+  console.log(195);
+}
+function foo196() {
+  console.log(196);
+}
+function foo197() {
+  console.log(197);
+}
+function foo198() {
+  console.log(198);
+}
+function foo199() {
+  console.log(199);
+}
+function foo200() {
+  console.log(200);
+}

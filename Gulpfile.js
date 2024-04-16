5d1e9b37006b931d7a836a88bd8aaeaca7482f80
Tsuyoshi Horo <horo@chromium.org>
2024-04-16 05:37:22 +0000

    Add demo of Dictionary registration error

--- Gulpfile.js
+++ Gulpfile.js
@@ -2,6 +2,7 @@ const { dest, parallel, series, src, watch } = require("gulp");
 const connect = require("gulp-connect");
 const del = require("delete");
 const less = require("gulp-less");
+const parseUrl = require("parseurl");
 const sass = require("gulp-sass")(require("sass"));
 const sourcemaps = require("gulp-sourcemaps");
 const ts = require("gulp-typescript");
@@ -47,6 +48,57 @@ const build = parallel(buildAssets, buildStyles, buildTypeScript);
 
 const clean = cb => del([`${DISTDIR}`], cb);
 
+function handleDictionaryRequest(req, res, next) {
+  const parsedUrl = parseUrl(req);
+  if (parsedUrl.pathname != "/dictionary") {
+    next();
+    return;
+  }
+  let use_as_dictionary = 'match="/test/*"';
+  let cache_control = "public, max-age=100";
+  switch (parsedUrl.query) {
+    case "OK":
+      break;
+    case "InvalidStructuredHeader":
+      use_as_dictionary = 'match="';
+      break;
+    case "MatchFieldIsNotAString":
+      use_as_dictionary = "match=test";
+      break;
+    case "MatchDestFieldIsNotAList":
+      use_as_dictionary = 'match="/test/*", match-dest=document';
+      break;
+    case "MatchDestFieldListItemIsNotAString":
+      use_as_dictionary = 'match="/test/*", match-dest=(document)';
+      break;
+    case "TypeFieldIsNotAToken":
+      use_as_dictionary = 'match="/test/*", type="raw"';
+      break;
+    case "IdFieldIsNotAString":
+      use_as_dictionary = 'match="/test/*", id=dict_id';
+      break;
+    case "IdFieldTooLong":
+      const long_id = "x".repeat(1025);
+      use_as_dictionary = `match="/test/*", id="${long_id}"`;
+      break;
+    case "NoMatchField":
+      use_as_dictionary = 'id="dict_id"';
+      break;
+    case "ExpiredResponse":
+      cache_control = "no-store";
+      break;
+    case "UnsupportedType":
+      use_as_dictionary = 'match="/test/*", type=unsupported';
+      break;
+    case "InvalidMatchField":
+      use_as_dictionary = 'match="("';
+      break;
+  }
+  res.setHeader("Use-As-Dictionary", use_as_dictionary);
+  res.setHeader("Cache-Control", cache_control);
+  res.end("Test dictionary");
+}
+
 exports.build = build;
 exports.clean = clean;
 exports.default = async () => {
@@ -54,6 +106,9 @@ exports.default = async () => {
   await connect.server({
     root: `${DISTDIR}`,
     livereload: true,
-    port: 8000
+    port: 8000,
+    middleware: function(connect, opt) {
+      return [handleDictionaryRequest];
+    }
   });
 };
